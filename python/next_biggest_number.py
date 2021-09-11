79dab42e8d0e63ef5699079ef4a5fcfc086820d8
Jeff H <jeffhcross@gmail.com>
2021-09-11 10:17:57 -0500

    Implemented next_biggest_number
    Using itertools permutations

--- python/next_biggest_number.py
+++ python/next_biggest_number.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
 import sys
+from itertools import permutations
 
 def main():
     next_biggest_number(sys.argv[1])
 
-
 def next_biggest_number(num):
-    #TODO: Implement me!
-    return 0
+    next_highest = sys.maxsize
+    for s in sorted(permutations(str(num))):
+        test_num = int(''.join(s))
+        if test_num <= num:
+            continue
+        elif test_num > num and test_num < next_highest:
+            next_highest = test_num
+            ## Since the list is sorted no higher number can be the result
+            break
+    if next_highest == sys.maxsize:
+        return -1
+    else:
+        return next_highest
 
 if __name__ == "__main__":
     main()
